5242c8c58e0af3b5bab2b37809662b319f674b2f
Matthew Sumner <matt.m.sumner@gmail.com>
2015-09-18 15:28:02 -0400

    Allow user to change the width and height

--- tests/acceptance/user-sees-a-board.js-test.js
+++ tests/acceptance/user-sees-a-board.js-test.js
@@ -12,12 +12,20 @@ module('Acceptance | user sees a board.js', {
   }
 });
 
-test('visiting / the user sees a game board', function(assert) {
+test('visiting / the user sees a game board and can change the width and height', function(assert) {
   visit('/');
 
   andThen(function() {
     assert.equal(gameBoardRows(), 10, 'there should be 10 rows');
     assert.equal(gameBoardCells(), 100, 'there should be 100 cells');
+  });
+
+  fillIn('#board-width', '20');
+  fillIn('#board-height', '30');
+
+  andThen(function() {
+    assert.equal(gameBoardRows(), 30, 'there should be 30 rows');
+    assert.equal(gameBoardCells(), 600, 'there should be 600 cells');
   });
 });
 
