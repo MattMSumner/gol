7e267a054d209fc758c058a36bc3c442c503c24b
Matthew Sumner <matt.m.sumner@gmail.com>
2015-09-22 12:32:27 -0400

    Added ember-cli-github-pages addon

--- config/environment.js
+++ config/environment.js
@@ -4,8 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'gol',
     environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
+    baseURL: '/gol',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
